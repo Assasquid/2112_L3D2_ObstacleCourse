a52de8d955b53a124301bf27d00e5c5b9915477d
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2021-12-25 21:19:30 +0000

    Set up the project and made the player move

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1;
+    Vector2 moveInput;
+    Rigidbody myRigidBody;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void Move()
+    {
+        Vector3 playerVelocity = new Vector3 (  moveInput.x * moveSpeed * Time.deltaTime, 
+                                                myRigidBody.velocity.y,     
+                                                moveInput.y * moveSpeed * Time.deltaTime);
+
+        myRigidBody.velocity = playerVelocity;
+    }
+}
